2b37c5695c1e5127b1ec05c9c3b7f2fd13b5f19d
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2022-02-22 14:22:19 +0100

    add envs

--- simpleapp/src/main/java/com/zhenik/app/simple/GreetingController.java
+++ simpleapp/src/main/java/com/zhenik/app/simple/GreetingController.java
@@ -4,15 +4,22 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private final Environment env;
     private static final Logger LOG = LoggerFactory.getLogger(GreetingController.class);
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    public GreetingController(Environment env) {this.env = env;}
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -32,5 +39,11 @@ public class GreetingController {
     public String logError() {
         LOG.info("Logging ERROR");
         return "Logging ERROR";
+    }
+
+    @GetMapping("/env/{key}")
+    public String getEnv(@PathVariable String key) {
+        LOG.info("Get key - {}", key);
+        return env.getProperty(key);
     }
 }
